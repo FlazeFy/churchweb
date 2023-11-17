66cbc118611a475f8583812b6e107aef57e86cea
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-11-17 18:31:16 +0700

    tidy up and header welcome

---
+++ webgereja/resources/views/components/bars/navbar.blade.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-expand-lg fixed-top shadow">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="logo-GKPI.png" alt="" style="height: 50px"></a>
+        <label class="navbar-brand" href="#" style="font-weight: bold"> <h3>GKPI Kota Tebing Tinggi</h3></label>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto align-items-center">
+            <li class="nav-item">
+                <a class="nav-link mx-2 active" href="/">Beranda</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Kategori
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="/rancangan">Rancangan Program</a></li>
+                    <li><a class="dropdown-item" href="/renungan">Renungan</a></li>
+                    <li><a class="dropdown-item" href="/kegiatan">Kegiatan</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Data Gereja
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="/jemaat">Jemaat</a></li>
+                    <li><a class="dropdown-item" href="/warta">Warta Jemaat</a></li>
+                    <li><a class="dropdown-item" href="/keuangan">Keuangan</a></li>
+                    <li><a class="dropdown-item" href="/statistik">Statistik Jemaat</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Tentang Gereja
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="/sejarah">Sejarah Gereja</a></li>
+                    <li><a class="dropdown-item" href="/struktur">Struktur Organisasi </a></li>
+                    <li><a class="dropdown-item" href="/badanpengurus">Badan Pengurus Harian</a></li>
+                </ul>
+            </li>
+            </ul>
+            <button class="btn btn-danger me-3 ms-2" > Diakora </button>
+            <a class="btn btn-primary" href="/login" > Masuk </a>
+        </div>
+    </div>
+</nav>
--- webgereja/resources/views/landing/index.blade.php
+++ webgereja/resources/views/landing/index.blade.php
@@ -6,67 +6,38 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>Gereja Kota Tebing Tinggi</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/328b2b4f87.js" crossorigin="anonymous"></script>
+
+    <!-- CSS Collection -->
+    <link rel="stylesheet" href="{{ asset('/css/global_v1.0.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/button_v1.0.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/navbar_v1.0.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/dropdown_v1.0.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/typography_v1.0.css') }}"/>
 </head>
 <body>
-    <!-- Navbar -->
-<nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
-    <div class="container">
-      <a class="navbar-brand" href="#"><img src="logo-GKPI.png" alt="" style="height: 50px"></a>
-      <label class="navbar-brand" href="#" style="font-weight: bold"> <h3>GKPI Kota Tebing Tinggi</h3></label>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto align-items-center">
-          <li class="nav-item">
-            <a class="nav-link mx-2" href="#!">Beranda</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Kategori
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Rancangan Program</a></li>
-              <li><a class="dropdown-item" href="#">Renungan</a></li>
-              <li><a class="dropdown-item" href="#">Kegiatan</a></li>
-            </ul>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Data Gereja
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Jemaat</a></li>
-              <li><a class="dropdown-item" href="#">Warta Jemaat</a></li>
-              <li><a class="dropdown-item" href="#">Keuangan</a></li>
-              <li><a class="dropdown-item" href="#">Statistik Jemaat</a></li>
-            </ul>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Tentang Gereja
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Sejarah Gereja</a></li>
-              <li><a class="dropdown-item" href="#">Struktur Organisasi </a></li>
-              <li><a class="dropdown-item" href="#">Badan Pengurus Harian</a></li>
-            </ul>
-          </li>
-        </ul>
-        <button class="btn btn-danger me-3 ms-2" > Diakora </button>
-        <button class="btn btn-primary" > Masuk </button>
+  <!-- Navbar -->
+  @include('components.bars.navbar')
+
+  <div class="main">
+    <div class="row mt-5">
+      <div class="col-lg-8 col-md-7 col-sm-12 pt-5">
+        <h1 class="mt-4">GKPI KOTA TEBING TINGGI</h1>
+        <h4>Melayani bukan untuk dilayani</h4>
+        <p class="m-0 mt-4">Mazmur 23:1</p>
+        <p class="m-0">TUHAN adalah gembalaku, takkan kekurangan aku.</p>
+      </div>
+      <div class="col-lg-4 col-md-5 col-sm-12">
+        <img src="{{ asset('/assets/papi_j.png') }}" alt="" class="img img-fluid d-block mx-auto">
       </div>
     </div>
-  </nav>
-  <!-- Navbar -->
+  </div>
+
 </body>
 </html>
