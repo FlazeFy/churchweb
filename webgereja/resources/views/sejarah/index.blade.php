c55bc79964d47dc6787ef72abb328c8f55b46592
aliyahuyy <58002736+aliyahuyy@users.noreply.github.com>
2023-11-20 14:49:10 +0700

    Sejarah Page

---
+++ webgereja/resources/views/sejarah/index.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sejarah Gereja Kota Tebing Tinggi</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/328b2b4f87.js" crossorigin="anonymous"></script> 
+
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Jquery -->
+    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
+    <!-- CSS Collection -->
+    <link rel="stylesheet" href="{{ asset('/css/global_v1.0.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/button_v1.0.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/navbar_v1.0.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/dropdown_v1.0.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/typography_v1.0.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/container_v1.0.css') }}"/>
+</head>
+<body>
+    <!-- Navbar -->
+  @include('components.bars.navbar')
+
+  <div class="main">
+    <br><br><br>
+    <div class="text-center">
+        @include('components.typographies.section_title', ['title'=>'Sejarah Gereja'])
+    </div>
+
+    <br><br><br>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 px-4" style="text-align: justify">
+            <p>
+                Gereja di segala abad dan tempat menghadapi berbagai tantangan dan pertanyaan, baik yang bersifat klasik maupun yang baru sejalan dengan perkembangan zaman dan konteksnya. 
+                Terhadap semua itu gereja harus memberi jawab, jawaban itu harus bersumber dari dan didasarkan pada Firman Tuhan di dalam Alkitab sebagai satu-satunya sumber ajaran dan norma yang benar.
+            </p>
+            <p>
+                Demikian juga Gereja Kristen Protestan Indonesia (GKPI). GKPI adalah bagian dari Tubuh Kristus yang esa, kudus, am dan rasuli di muka bumi ini. 
+                GKPI adalah juga suatu persekutuan yang secara khas hadir di negara Republik Indonesia ini. Sejak berdirinya tanggal 30 Agustus 1964. 
+                GKPI ditantang untuk memberi jawab atas berbagai pertanyaan dan masalah yang menyangkut banyak hal mendasar yang diimaninya, dalam rangka menyatakan ketaatan dan kesetiaannya kepada Tuhan Yesus Kristus,  Raja Gereja dan Juruselamat dunia.
+            </p>
+            <p>
+                Ada berbagai  jawaban yang telah dikemukakan dan dirumuskan GKPI di sepanjang sejarah dan perjalanannya. 
+                Jawaban dan pernyataan iman itu tersebar di berbagai dokumen, berupa keputusan-keputusan dan pesan-pesan Sinode Am dan Rapat Pendeta, maupun berbagai hasil pertemuan dan persidangan di semua aras (tingkatan), dari tingkat Jemaat hingga Pusat. 
+                Karena sifat dan bentuknya tersebar dan tidak tersusun padu, GKPI perlu menyusun dan memiliki  rumusan yang lebih lengkap dan terpadu, agar dapat dipedomani dan digunakan seluruh warga dan pelayan GKPI tatkala menghadapi berbagai pertanyaan dan masalah yang menyangkut iman mereka.
+            </p>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 px-4" style="text-align: justify">
+            <p>Dengan demikian Pokok Pokok Pemahaman Iman GKPI ini berfungsi sebagai: </p>
+            <ol>
+                <li>Pedoman dan tuntunan bagi seluruh jajaran GKPI dalam hal ajaran, supaya tidak ?diombang ambingkan oleh rupa rupa angin pengajaran? (ef 4:14) dan supaya dapat menjawab perkara-perkara mendasar yang berkaitan dengan imannya, sebagaimana terlihat dari susunan isi dokumen ini;</li>
+                <li>Pedoman  dan acuan bagi warga dan pelayan GKPI dalam merumuskan bentuk, isi, dan tujuan kesaksian dan pelayanan, demikian juga dalam menyusun peraturan dan program GKPI disemua aras</li>
+                <li> Pedoman dan acuan untuk memberi jawab dan pertanggungjawaban tentang imannya terhadap berbagai pengajaran dan nilai yang terus menerus bermunculan dan berubah, sekaligus menolaknya kalau ternyata bertentangan dengan iman kristiani. Pokok pokok Pemahaman Iman GKPI ini bersumber dan berdasar pada Alkitab. Karena itu tidak dimaksudkan sebagai pengganti ataupun tandingan Alkitab. Namun di lain pihak dokumen ini hendak juga mencerminkan pemahaman dan penafsiran atas amanat Alkitab secara aktual, sehingga Alkitab sungguh-sungguh bersuara secara relevan pada masa kini, dan pesannya menjadi jelas bagi para warga dan pelayan GKPI.</li>
+            </ol>
+            <p>
+                Pokok pokok Pemahaman Iman GKPI ini juga mengacu pada sejumlah dokumen iman gereja dari segala abad, terutama yang dipelihara dalam tradisi iman gereja reformatoris, antara lain Pengakuan Iman Rasuli, Pengakuan Iman Nicea-Konstantinopel, Pengakuan Iman Athanasianum, Katekhismus Martin Luther dan Konfessi Augsburg, demikian juga Pokok pokok ajaran bapa bapa gereja yang diakui sebagai bersifat ekumenis dan reformatoris. Dan sebagai bagian dari gereja Kristen yang mengesa di Indonesia, Pokok pokok Pemahaman iman ini juga mengacu kepada Pemahaman Bersama Iman Kristen di Indonesia (PBIK), salah satu dari Lima Dokumen Keesaan Gereja (LDKG) yang dirumuskan bersama di dalam Persekutuan Gereja di Indonesia (PGI).
+            </p>
+            <p>
+            Seperti juga dinyatakan dalam Tata Gereja GKPI, dengan pokok Pokok Pemahaman Iman ini GKPI juga sekaligus hendak menegaskan jatidirinya sebagai gereja yang hadir di tengah pergumulan bangsanya, dan turut memberi sumbangan bagi peletakan landasan moral, etik dan spritual bagi kehidupan bangsa Indonesia yang sedang membangun demi perwujudan cita-citanya.
+            </p>
+        </div>
+    </div>
+    
+  </div>
+
+  <!-- Footer -->
+  @include('components.bars.footer')
+</body>
+</html>
